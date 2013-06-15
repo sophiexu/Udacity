065f20e8b2ca48834443d2969aff8a6cbd968fbe
Megan Marsh <megan.n.marsh@gmail.com>
2013-06-14 20:34:34 -0700

    added unit 3 homeworks

--- days_old2.py
+++ days_old2.py
@@ -54,22 +54,21 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     if isLeapYear(year2) and month2 < 3: # you should subtract a leap day because you counted one too many
         # print 'subtracting day from day2'
         days = days - 1
-
+    print days
     return days
 
     # Test routine
-
-def test():
-    test_cases = [((2012,1,1,2012,2,28), 58), 
-                  ((2012,1,1,2012,3,1), 60),
-                  ((2011,6,30,2012,6,30), 366),
-                  ((2011,1,1,2012,8,8), 585 ),
-                  ((1900,1,1,1999,12,31), 36523)]
-    for (args, answer) in test_cases:
-        result = daysBetweenDates(*args)
-        if result != answer:
-            print "Test with data:", args, "failed"
-        else:
-            print "Test case passed!"
-
-test()
+#def test():
+#    test_cases = [((2012,1,1,2012,2,28), 58), 
+#                  ((2012,1,1,2012,3,1), 60),
+#                  ((2011,6,30,2012,6,30), 366),
+#                  ((2011,1,1,2012,8,8), 585 ),
+#                  ((1900,1,1,1999,12,31), 36523)]
+#    for (args, answer) in test_cases:
+#        result = daysBetweenDates(*args)
+#        if result != answer:
+#            print "Test with data:", args, "failed"
+#        else:
+#            print "Test case passed!"
+#
+#test()
